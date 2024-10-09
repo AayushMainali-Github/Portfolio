e132d874d02a39db415bdd99888fc9de49612c60
New 6969 <aayushmainali.codes@gmail.com>
2024-10-09 17:03:43 +0545

    Portfolio Update

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="head">Hey there!</div>
       <div className="desc">
         I am Aayush Mainali from Nepal, A web developer who loves doing random things on the web. I have developed a few things here and there on the web. Not that good of a
-        designer but I try my best! I mostly use MERN stack with Typescript and React Native for most of my projects. Currently working at Enzyme AI.
+        designer but I try my best! I mostly use MERN stack with Typescript and React Native for most of my projects.
       </div>
     </div>
   );
